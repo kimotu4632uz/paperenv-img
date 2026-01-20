e5889080b965a09c4a26012049804d6c23bcbbe1
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2026-01-20 12:37:42 +0900

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if type devcontainer >/dev/null 2>&1; then
-    devcontainer build , --image-name paperenv-img
+    devcontainer build . --image-name paperenv-img
 else
     echo "devcontainer CLI not found. Please install it from command palette in VSCode."
     exit 1
